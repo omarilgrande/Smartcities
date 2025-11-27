d8d798d5cc98e1953daa7d9f86e9b6609db98ebf
Hepl-Bennanna <omar.bennanna@student.hepl.be>
2025-11-27 16:34:04 +0100

    Update TablesMariaDB.py

--- Projet Final/TablesMariaDB.py
+++ Projet Final/TablesMariaDB.py
@@ -6,19 +6,16 @@ class Base(DeclarativeBase):
 
 class Image(Base):
     __tablename__ = "Images"
-    idi = mapped_column(Integer, primary_key = True)
+    id = mapped_column(Integer, primary_key = True)
     path = mapped_column(String(40),nullable=False)
     date = mapped_column(String(20),nullable=False)
-    #couleur= mapped_column(Integer,nullable=False)
-    #taille= mapped_column(Integer,nullable=False)
-    #luminosite= mapped_column(Integer,nullable=False)
 
     def __str__(self):
         return f"{self.idi}:{self.path},{self.date}"
     
 class Battrie(Base):
     __tablename__ = "Battrie"
-    idb = mapped_column(Integer, primary_key = True)
+    id = mapped_column(Integer, primary_key = True)
     poucentage = mapped_column(Integer,nullable=False)
     date = mapped_column(String(20),nullable=False)
 
@@ -27,12 +24,38 @@ class Battrie(Base):
     
 class Temperature(Base):
     __tablename__ = "Temperature"
-    idt = mapped_column(Integer, primary_key = True)
+    id = mapped_column(Integer, primary_key = True)
     temperature = mapped_column(String(40),nullable=False)
     date = mapped_column(String(20),nullable=False)
 
     def __str__(self):
         return f"{self.idt}:{self.temperature},{self.date}"
+
+class Wifi(Base):
+    __tablename__ = "Wifi"
+    id = mapped_column(Integer, primary_key = True)
+    ssid = mapped_column(String(40),nullable=False)
+    pasword = mapped_column(String(40),nullable=False)
+    date = mapped_column(String(20),nullable=False)
+
+
+    def __str__(self):
+        return f"{self.id}:{self.ssid},{self.pasword}"
+    
+class CamParam(Base):
+    __tablename__ = "CamParametres"
+    id = mapped_column(Integer, primary_key = True)
+    resolution = mapped_column(String(50), nullable=False)
+    brightness = mapped_column(Integer, nullable=False)
+    contrast = mapped_column(Integer, nullable=False)
+    saturation = mapped_column(Integer, nullable=False)
+    quality = mapped_column(Integer, nullable=False)
+    mirror = mapped_column(String(5), nullable=True)
+    flip = mapped_column(String(5), nullable=True)
+    date = mapped_column(String(20),nullable=False)
+
+    def __str__(self):
+        return f"{self.id}:{self.resolution},{self.brightness},{self.contrast},{self.saturation},{self.quality},{self.mirror},{self.flip}"
     
 def main():
     engine = create_engine("mariadb+mariadbconnector://martin:1234@192.168.2.45:3306/RPG", echo = True)
